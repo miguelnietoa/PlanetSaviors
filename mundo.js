48a2019f2b2c91db7a9a1453fcdabc00ccb6df21
Miguel NA <miguelnietoarias3@gmail.com>
2019-11-07 16:50:15 -0500

    Generar correctamente los items y ya estan los contenedores

--- mundo.js
+++ mundo.js
@@ -17,59 +17,65 @@ const config = {
 var game = new Phaser.Game(config);
 var timer;
 var cont = 0;
-var libros = [];
-var bananas = [];
-var tomates = [];
+var items = [];
 
 function init() {
 
 }
 
 function preload() {
-    this.load.image("ContenedorRojo", "./assets/e-WasteBin/binE-Waste.png");
-    this.load.image("Calculadora","./assets/e-WasteBin/calculator_1.png" );
-    this.load.image("foco","./assets/e-WasteBin/foco.png");
-    this.load.image("microondas","./assets/e-WasteBin/microondas.png");
-    this.load.image("BateriaOne","./assets/e-WasteBin/Recurso 10.png");
-    this.load.image("BateriaTwo","./assets/e-WasteBin/Recurso 11.png");
-    this.load.image("ContenedorAzul","./assets/glassBin/binGlass.png");
-    this.load.image("VidrioOne","./assets/glassBin/Recurso 3.png");
-    this.load.image("VidrioTwo","./assets/glassBin/Recurso 14.png");
-    this.load.image("VidrioThree","./assets/glassBin/Recurso 27.png");
-    this.load.image("VidrioFour","./assets/glassBin/Recurso 28.png");
-    this.load.image("Metalbin","./assets/metalBin/binMetal.png");
-    this.load.image("MetalOne","./assets/metalBin/lata.png");
-    this.load.image("MetalTwo","./assets/metalBin/lata1.png");
-    this.load.image("MetalThree","./assets/metalBin/lata2.png");
-    this.load.image("MetalFour","./assets/metalBin/lata3.png");
-    this.load.image("MetalFive","./assets/metalBin/lata4.png");
-    this.load.image("MetalSix","./assets/metalBin/Recurso 9.png");
-    this.load.image("MetalSeven","./assets/metalBin/tijera.png");
-    this.load.image("OrganicBin","./assets/organicBin/organicc_1.png");
-    this.load.image("OrganicOne","./assets/organicBin/bone.png");
-    this.load.image("OrganicTwo","./assets/organicBin/pimentorn2.png");
-    this.load.image("OrganicThree","./assets/organicBin/Recurso 5.png");
-    this.load.image("OrganicFour","./assets/organicBin/Recurso 6.png");
-    this.load.image("OrganicFive","./assets/organicBin/Recurso 7.png");
-    this.load.image("OrganicSix","./assets/organicBin/Recurso 22.png");
-    this.load.image("OrganicSeven","./assets/organicBin/Recurso 23.png");
-    this.load.image("OrganicEight","./assets/organicBin/zanahoria.png");
-    this.load.image("PaperBin","./assets/paperBin/binPaper.png");
-    this.load.image("PaperOne","./assets/paperBin/book.png");
-    this.load.image("PaperTwo","./assets/paperBin/Recurso 18.png");
-    this.load.image("PaperThree","./assets/paperBin/Recurso 20.png");
-    this.load.image("PaperFour","./assets/paperBin/Recurso 21.png");
-    this.load.image("Plasticbin","./assets/plasticBin/binPlastic.png");
-    this.load.image("PlasticOne","./assets/plasticBin/Recurso 4.png");
-    this.load.image("PlasticTwo","./assets/plasticBin/Recurso 32.png");
-    this.load.image("PlasticThree","./assets/plasticBin/Recurso 33.png");
-    this.load.image("PlasticFour","./assets/plasticBin/Recurso 35.png");
-  
+    this.load.image("eWasteBin", "./assets/e-WasteBin/binE-Waste.png");
+    this.load.image("eWaste1", "./assets/e-WasteBin/calculator_1.png");
+    this.load.image("eWaste2", "./assets/e-WasteBin/foco.png");
+    this.load.image("eWaste3", "./assets/e-WasteBin/microondas.png");
+    this.load.image("eWaste4", "./assets/e-WasteBin/Recurso 10.png");
+    this.load.image("eWaste5", "./assets/e-WasteBin/Recurso 11.png");
+    this.load.image("glassBin", "./assets/glassBin/binGlass.png");
+    this.load.image("glass1", "./assets/glassBin/Recurso 3.png");
+    this.load.image("glass2", "./assets/glassBin/Recurso 14.png");
+    this.load.image("glass3", "./assets/glassBin/Recurso 27.png");
+    this.load.image("glass4", "./assets/glassBin/Recurso 28.png");
+    this.load.image("metalBin", "./assets/metalBin/binMetal.png");
+    this.load.image("metal1", "./assets/metalBin/lata.png");
+    this.load.image("metal2", "./assets/metalBin/lata1.png");
+    this.load.image("metal3", "./assets/metalBin/lata2.png");
+    this.load.image("metal4", "./assets/metalBin/lata3.png");
+    this.load.image("metal5", "./assets/metalBin/lata4.png");
+    this.load.image("metal6", "./assets/metalBin/Recurso 9.png");
+    this.load.image("metal7", "./assets/metalBin/tijera.png");
+    this.load.image("organicBin", "./assets/organicBin/organicc_1.png");
+    this.load.image("organic1", "./assets/organicBin/bone.png");
+    this.load.image("organic2", "./assets/organicBin/pimenton2.png");
+    this.load.image("organic3", "./assets/organicBin/Recurso 5.png");
+    this.load.image("organic4", "./assets/organicBin/Recurso 6.png");
+    this.load.image("organic5", "./assets/organicBin/Recurso 7.png");
+    this.load.image("organic6", "./assets/organicBin/Recurso 22.png");
+    this.load.image("organic7", "./assets/organicBin/Recurso 23.png");
+    this.load.image("organic8", "./assets/organicBin/zanahoria.png");
+    this.load.image("paperBin", "./assets/paperBin/binPaper.png");
+    this.load.image("paper1", "./assets/paperBin/book.png");
+    this.load.image("paper2", "./assets/paperBin/Recurso 18.png");
+    this.load.image("paper3", "./assets/paperBin/Recurso 20.png");
+    this.load.image("paper4", "./assets/paperBin/Recurso 21.png");
+    this.load.image("plasticBin", "./assets/plasticBin/binPlastic.png");
+    this.load.image("plastic1", "./assets/plasticBin/Recurso 4.png");
+    this.load.image("plastic2", "./assets/plasticBin/Recurso 32.png");
+    this.load.image("plastic3", "./assets/plasticBin/Recurso 33.png");
+    this.load.image("plastic4", "./assets/plasticBin/Recurso 35.png");
+
 }
 
 function create() {
     // this.cameras.main.backgroundColor.setTo(255,255,255); // Color de fondo de la escena
+    console.log(this.add.image(80, 110, 'glassBin').setScale(0.8).width); 
+    this.add.image(80, 110*3, 'plasticBin').setScale(0.75);
+    this.add.image(80, 110*5, 'metalBin').setScale(0.75);
+    this.add.image(1220, 110, 'paperBin').setScale(0.8);
+    this.add.image(1220, 110*3, 'organicBin').setScale(0.75);
+    this.add.image(1220, 110*5, 'eWasteBin').setScale(0.75);
+    
 
+    
     this.input.on('dragstart', function (pointer, gameObject) { // Empieza a arrastrar
         gameObject.body.setVelocityY(0);
         gameObject.tint = Math.random() * 0xffffff;
@@ -88,19 +94,66 @@ function create() {
 
 function update() {
 
-    creacionDeItem(this);
-    isOnFloor(bananas);
-    isOnFloor(tomates);
-    isOnFloor(libros);
+    if (items.length != 15) { // Generando items
+        let random = Phaser.Math.Between(0, 5);
+        let item;     
+        random = 3;   
+        switch (random) { // VA EN 200
+            case 0: // eWasteBin (5)
+            
+                item = new itemJuego(this, 200, 0, 30, "eWaste" + Phaser.Math.Between(1, 5));
+                break;
+            case 1: // glassBin (4)
+                item = new itemJuego(this, Phaser.Math.Between(160, 1100), 0, 30, "glass" + Phaser.Math.Between(1, 4));
+                break;
+            case 2: // metalBin (7)s
+                item = new itemJuego(this, Phaser.Math.Between(160, 1100), 0, 30, "metal" + Phaser.Math.Between(1, 7));
+                break;
+            case 3: // organicBin (8)
+                item = new itemJuego(this, Phaser.Math.Between(160, 1100), 0, 30, "organic" +Phaser.Math.Between(1, 8));
+                break;
+            case 4: // paperBin (4)
+                item = new itemJuego(this, Phaser.Math.Between(160, 1100), 0, 30, "paper" + Phaser.Math.Between(1, 4));
+                break;
+            case 5: // plasticBin (4)
+                item = new itemJuego(this, Phaser.Math.Between(160, 1100), 0, 30, "plastic" + Phaser.Math.Between(1, 4));
+                break;
+        }
+        if (item !== undefined) {
+            if (colisionaArray(item, items)) {
+                item.destroy();
+            } else {
+                items.push(item);
+            }
+        }
+
+
+    }
+    isOnFloor(items);
 
 }
 
+function colisionaArray(item, items) {
+    let sw = false;
+    let i = 0;
+    while (i < items.length && !sw) {
+        if (colisiona(items[i], item)) {
+            sw = true;
+        }
+        i++;
+    }
+    return sw;
+}
+
 function isOnFloor(array) {
+    let i = 0;
     array.forEach(item => {
-        if (item.y + item.height / 2 >= 650){ // Si está en el piso
+        if (item.y + item.height / 2 >= 650) { // Si está en el piso
             item.body.setVelocityY(0); // Se queda quieto
             item.input.draggable = false;  // No se puede arrastrar
+            items.splice(i, 1);
         }
+        i++;
     });
 
 
@@ -113,41 +166,16 @@ function updateCounter() {
     }
 }
 
-function creacionDeItem(scene) {
-    let random = Phaser.Math.Between(0, 2);
-    switch (random) {
-        case 0:
-            if (libros.length != 10) {
-                let rnd = Phaser.Math.Between(0, 1300);
-                let sw = false;
-                libros.forEach(item => {
-                    if (Math.abs(item.x - rnd) <= item.width) {
-                        sw = true;
-                    }
-                });
-                if (!sw) {
-                    libros.push(new itemJuego(scene, rnd, -30, 10, "libro"));
-                }
-
-            }
-
-            break;
-        case 1:
-            if (tomates.length != 5) {
-                tomates.push(new itemJuego(scene, Phaser.Math.Between(0, 1300), -30, 20, "tomate"));
-
-            }
-            break;
-        case 2:
-            if (bananas.length != 2) {
-                bananas.push(new itemJuego(scene, Phaser.Math.Between(0, 1300), -30, 30, "banana"));
-            }
-            break;
-    }
-
-
-}
-
 function colisiona(item1, item2) {
-    
+    // Se hace el ajuste para que las posiciones sean desde la esquina superior izquierda
+    return colision(item1.x - item1.width / 2, item1.y - item1.height / 2, item1.width, item1.height, item2.x - item2.width / 2, item2.y - item2.height / 2, item2.width, item2.height);
 }
+
+function colision(x1, y1, w1, h1, x2, y2, w2, h2) {
+    if (x1 < x2 + w2 && x1 + w1 > x2 &&
+        y1 < y2 + h2 && h1 + y1 > y2) {
+        return true;
+    }
+    return false;
+}
+
